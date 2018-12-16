77bdb4038340101024ae34e9e52fcf078b347474
seliugin <45312976+seliugin@users.noreply.github.com>
2018-12-16 16:00:33 +0500

    Add brackets header

---
+++ brackets/brackets.h
@@ -0,0 +1,29 @@
+/*
+ * This file is brackets.h
+ * Header for stack implementation
+ */
+
+#pragma once
+
+// structures declaration
+typedef struct bracket* pbracket;
+
+struct bracket
+{
+	char value;
+	pbracket next;
+};
+
+/*
+ * Push a bracket to stack
+ * Params: top - pointer to the top bracket, new_value - value we want to add
+ * Returns: none
+ */
+void push(pbracket top, int new_value);
+
+/* 
+ * Pop a bracket from the stack
+ * Params: top - pointer to the top bracket
+ * Returns the popped bracket
+ */
+int pop(pbracket top);
